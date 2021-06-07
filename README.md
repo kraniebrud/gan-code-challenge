f9ccca1ceecb1db6f8b41e17577063eaadcfc359
kraniebrud <kraniebrud@users.noreply.github.com>
2021-06-07 09:51:57 +0200

    should run to the end. added all-cities.json to .gitignore and wrote up a readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# GAN code challenge
+
+## Running it
+
+There are 2 ways to try the challenge
+
+1. start server and then start the challenge
+    * `npm run start-server` the server will start
+    *  in a new terminal window `npm run start-challenge`
+2. using "spin.js" and `npm start`
+    * `npm start` it will start server when server is ready it should run the challenge and print to out
+
+## Notes
+
+I decided to keep it as slim as possible, leaving out tests, error handling, code style convention, schema etc. Focusing mainly on having the challenge to run all the way to the end with clear code. Please note that the completion is *not* what I would call production ready! 
+
+.. I mistyped company name for the repo, sorry!
--- server/index.js
+++ server/index.js
@@ -1,30 +1,19 @@
-const fastify = require('fastify')()
+const fastify = require('fastify')({ logger: 'info' })
 const path = require('path')
 const fs = require('fs-extra')
 
 const PORT = '8080'
 const HOST = '127.0.0.1'
 const SECRET_TOKEN = Buffer.from('thesecrettoken').toString('base64')
-
-fastify.addHook('onRequest', async (req, reply) => {
-  const { headers } = req
-  const bearer = headers.authorization?.substring('bearer '.length) || ''
-  if (bearer === SECRET_TOKEN) {
-    return {}
-  }
-  return reply.code(401).send()
-})
+const ADDRESSES_FILE = path.resolve(__dirname, '..', 'challenge/addresses.json')
 
 const getAllAddresses = async () => {
-  const fpath = path.resolve(__dirname, '..', 'challenge/addresses.json')
-  const file = await fs.readFile(fpath)
-  return JSON.parse(file)
+  const data = await fs.readFile(ADDRESSES_FILE)
+  return JSON.parse(data)
 }
 
-const calcDist = (from, to) => {
-  /**
-   * https://www.movable-type.co.uk/scripts/latlong.html
-   */
+const calculateDistance = (from, to) => {
+  // > https://www.movable-type.co.uk/scripts/latlong.html, -- thank GOD for Google! ;)
 
   const [ lat1, lat2 ] = [ from.latitude, to.latitude ]
   const [ lon1, lon2 ] = [ from.longitude, to.longitude ]
@@ -41,38 +30,93 @@ const calcDist = (from, to) => {
             Math.sin(Δλ/2) * Math.sin(Δλ/2)
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
 
-  const dist = R * c // in metres
-  const km = (dist / 1000).toFixed(2) // to km & fixed 2 decimals
+  const distance = R * c // in metres
+  const km = (distance / 1000).toFixed(2) // to km & fixed 2 decimals
 
   return Number(km)
 }
 
+fastify.addHook('onRequest', async (req, reply) => {
+  const { headers } = req
+  const bearer = headers.authorization?.substring('bearer '.length) // go ahead and replace 'bearer' with 'cheese' if you want ;)
+  const authenticated = bearer && bearer === SECRET_TOKEN
+  if (!authenticated) {
+    reply.status(401).send()
+  }
+  return
+})
+
 fastify.get('/cities-by-tag', async (req, reply) => {
   const { query } = req
-
   const addresses = await getAllAddresses()
-
   const cities = addresses.filter((addr) => { 
     return addr.tags.includes(query.tag) && addr.isActive === Boolean(query.isActive)
   })
-
-  reply.code(200).send({ cities })
+  return { cities }
 })
 
 fastify.get('/distance', async (req, reply) => {
   const { query } = req
-
   const addresses = await getAllAddresses()
 
   const from = addresses.find((f) => f.guid === query.from)
   const to = addresses.find((f) => f.guid === query.to)
-  const distance = calcDist(from, to)
+  const distance = calculateDistance(from, to)
 
-  const result = { from, to, distance, unit: 'km' }
+  return { from, to, distance, unit: 'km' }
+})
 
-  reply.code(200).send(result)
+const inMemoryAreaQueue = {
+  queue: [],
+  enqueue: async ({ from, distance }) => {
+    const allAddresses = await getAllAddresses()
+    const fromAddress = allAddresses.find((f) => f.guid === from)
+    const result = allAddresses.filter((toAdress) => {
+      const x = calculateDistance(fromAddress, toAdress)
+      return x <= distance && toAdress.guid !== fromAddress.guid
+    })
+    return result
+  },
+  create: (area) => {
+    const guid = '2152f96f-50c7-4d76-9e18-f7033bd14428'
+    const enqueue = inMemoryAreaQueue.enqueue(area)
+    inMemoryAreaQueue.queue.push({ guid, enqueue })
+    return { guid }
+  },
+  get: (guid) => {
+    const enqueued = inMemoryAreaQueue.queue.find((f) => f.guid === guid)
+    return enqueued
+  },
+}
+
+fastify.get('/area', (req, reply) => {
+  const { protocol, hostname, query } = req
+  const { guid } = inMemoryAreaQueue.create({ from: query.from, distance: query.distance })
+  const resultsUrl = `${protocol}://${hostname}/area-result/${guid}`
+
+  reply.code(202)
+  return { resultsUrl }
+})
+
+fastify.get('/area-result/:guid', async (req, reply) => {
+  const { params } = req
+  const enqueued = inMemoryAreaQueue.get(params.guid)?.enqueue
+  if (enqueued) {
+    const cities = await enqueued
+    return { cities }
+  }
+  reply.code(202)
+  return []
+})
+
+fastify.get('/all-cities', async (req, reply) => {
+  const file = path.resolve(ADDRESSES_FILE)
+  const data = fs.createReadStream(file, 'utf8')
+  return data
 })
 
 fastify.listen(PORT, HOST, (err) => {
   if (err) throw err
 })
+
+module.exports.ready = fastify.ready
